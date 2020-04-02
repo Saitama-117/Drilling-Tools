293d60182a84285d227c4f3f215edaab36a4d570
Saitama-117 <60344345+Saitama-117@users.noreply.github.com>
2020-04-02 12:13:44 +0100

    Add files via upload
    loading about us, but still missing photos

--- about.php
+++ about.php
@@ -18,37 +18,44 @@ session_start();
     <!Sections BEGINS>
     <section id="about" class="section clear">
         <h1>ABOUT US</h1>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+        <div class="w3-container w3-padding-64 w3-center" id="team">
+    <h2>SUPER AWESOME TEAM</h2>
+    <p>Meet the people behind this great project:</p>
+    
+    <div class="w3-row"><br>
+    
+    <div class="w3-quarter">
+      <img src="https://i.imgur.com/AqGDAeJ.gif" alt="CEO" style="width:45%" class="w3-circle w3-hover-opacity">
+      <h3>Nick Harle</h3>
+      <p>CEO and Brain Master</p>
+    </div>
+    
+    <div class="w3-quarter">
+      <img src="https://i.imgur.com/FtJJVvg.jpg" alt="Sales" style="width:45%" class="w3-circle w3-hover-opacity">
+      <h3>Adeleke Omotayo Awonuga</h3>
+      <p>Sales and Social Support</p>
+    </div>
+    
+    <div class="w3-quarter">
+      <img src="https://pbs.twimg.com/profile_images/906558208549937152/z8n8s7-m_400x400.jpg" alt="Tester" style="width:45%" class="w3-circle w3-hover-opacity">
+      <h3>Abhinav Peddi</h3>
+      <p>Tester and Deungeon Developer</p>
+    </div>
+    
+    <div class="w3-quarter">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Aziz_Ansari_2012_Shankbone.JPG/800px-Aziz_Ansari_2012_Shankbone.JPG" alt="Developer" style="width:45%" class="w3-circle w3-hover-opacity">
+      <h3>Hari Saravanan Mohandoss</h3>
+      <p>PHP, JASON Developer and Party Master</p>
+    </div>
 
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-
-        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-
-        <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
-
-        <p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
-
-        <p>Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</p>
-
-        <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
-
-        <p>Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-
-        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-
-        <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
-
-        <p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
-
-        <p>Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</p>
-
-        <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
-
-        <p>Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
+    <div class="w3-quarter">
+        <img src="https://i.imgur.com/PAoXOfm.jpg" alt="Pro-gamer" style="width:45%" class="w3-circle w3-hover-opacity">
+        <h3>Luis Cruz Kim</h3>
+        <p>Front-End Wanna be Developer and Semi Pro-gamer</p>
+      </div>
+    
+    </div>
+    </div>
     </section>
 </main>
 <!FOOTER BEGIN>
