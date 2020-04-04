9adcf2c075777ef6ef06c6efa4e9da63a3c80845
Saitama-117 <60344345+Saitama-117@users.noreply.github.com>
2020-04-04 17:43:20 +0100

    Update about.php
    now with our pictures

--- about.php
+++ about.php
@@ -25,31 +25,31 @@ session_start();
     <div class="w3-row"><br>
     
     <div class="w3-quarter">
-      <img src="https://i.imgur.com/AqGDAeJ.gif" alt="CEO" style="width:45%" class="w3-circle w3-hover-opacity">
+      <img src="./Assets/HTML/images/Profile/Nick.png" alt="CEO" style="width:45%" class="w3-circle w3-hover-opacity">
       <h3>Nick Harle</h3>
       <p>CEO and Brain Master</p>
     </div>
     
     <div class="w3-quarter">
-      <img src="https://i.imgur.com/FtJJVvg.jpg" alt="Sales" style="width:45%" class="w3-circle w3-hover-opacity">
+      <img src="./Assets/HTML/images/Profile/Leke.png" alt="Sales" style="width:45%" class="w3-circle w3-hover-opacity">
       <h3>Adeleke Omotayo Awonuga</h3>
       <p>Sales and Social Support</p>
     </div>
     
     <div class="w3-quarter">
-      <img src="https://pbs.twimg.com/profile_images/906558208549937152/z8n8s7-m_400x400.jpg" alt="Tester" style="width:45%" class="w3-circle w3-hover-opacity">
+      <img src="./Assets/HTML/images/Profile/Abhinav.jpg" alt="Tester" style="width:45%" class="w3-circle w3-hover-opacity">
       <h3>Abhinav Peddi</h3>
       <p>Tester and Deungeon Developer</p>
     </div>
     
     <div class="w3-quarter">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Aziz_Ansari_2012_Shankbone.JPG/800px-Aziz_Ansari_2012_Shankbone.JPG" alt="Developer" style="width:45%" class="w3-circle w3-hover-opacity">
+      <img src="./Assets/HTML/images/Profile/Hari.jpg" alt="Developer" style="width:45%" class="w3-circle w3-hover-opacity">
       <h3>Hari Saravanan Mohandoss</h3>
       <p>PHP, JASON Developer and Party Master</p>
     </div>
 
     <div class="w3-quarter">
-        <img src="https://i.imgur.com/PAoXOfm.jpg" alt="Pro-gamer" style="width:45%" class="w3-circle w3-hover-opacity">
+        <img src="./Assets/HTML/images/Profile/Luis2.png" alt="Pro-gamer" style="width:45%" class="w3-circle w3-hover-opacity">
         <h3>Luis Cruz Kim</h3>
         <p>Front-End Wanna be Developer and Semi Pro-gamer</p>
       </div>
