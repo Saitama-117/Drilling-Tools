3427c679628196424bfffb239a398127586830e5
Leke Awonuga <a.awonuga@rgu.ac.uk>
2020-04-16 10:43:17 +0100

    html updates on CAD

--- Assets/JavaScript/FindTubular.js
+++ Assets/JavaScript/FindTubular.js
@@ -48,10 +48,11 @@ function searchTooltoDatabase()
                         htmlCode+="OD: "+tool["OD"]+" in, Min Temp: "+tool["minTemp"]+" &#8451, Max Temp: "+tool["maxTemp"]+" &#8451, ";
                         htmlCode+="Min Pressure: "+tool["minPressure"]+" psi, Max Pressure: "+tool["maxPressure"]+"psi</p>";
                         if (tool["CADurl"]) {
-                            htmlCode+="<iframe id='3dviewerplayer' type='text/html' width='640' height='480' ";
+                            htmlCode+="<iframe id='3dviewerplayer' type='text/html' width='640' height='480' "; style='margin:auto';
                             htmlCode+="src='"+tool["CADurl"]+"' frameborder='0' scrolling='no' allowfullscreen webkitallowfullscreen mozallowfullscreen><p>Your browser does not support iframes.</p></iframe>";
                         }
-                        htmlCode+="<hr>";
+
+                        htmlCode+="<hr class='one'>"; //horizontal lines btw tools
                         $("#TOOLS").append(htmlCode);
                     }
                 } else {
